38842609066db553851b8b806d52dcde14128f71
Mike Rydstrom <mrydstrom+gh@gmail.com>
2021-02-02 04:29:25 +0200

    Added web and desktop runners

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, ScopedReader watch) {
-    const FlexScheme usedFlexScheme = FlexScheme.hippieBlue;
+    const FlexScheme usedFlexScheme = FlexScheme.shark;
     final ThemeMode themeMode = watch(themeModeProvider).state;
 
     return MaterialApp(
