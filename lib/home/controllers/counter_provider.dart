4af5312d4f56b387c6afb2417a5ff8103c20cb55
Rydmike <m.rydstrom@gmail.com>
2022-10-28 19:04:08 +0300

    Change counterProver from StateProvider to NotifierProvider

--- lib/home/controllers/counter_provider.dart
+++ lib/home/controllers/counter_provider.dart
@@ -1,10 +1,13 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-// StateProvider used as controller for the counter.
+import '../model/counter.dart';
+
+// NotifierProvider used as controller for the counter.
 //
 // This one is not persisted. We want to get a counter starting from zero
 // when we start the app.
-final StateProvider<int> counterProvider = StateProvider<int>(
-  (final StateProviderRef<int> ref) => 0,
+final NotifierProvider<Counter, int> counterProvider =
+    NotifierProvider<Counter, int>(
+  Counter.new,
   name: 'counterProvider',
 );
